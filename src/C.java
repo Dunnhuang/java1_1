12df7bb02d2826b792df8e2f48f2bf08b54e9afa
Dunnhuang <Dunnhuang@163.com>
2020-04-01 19:08:05 +0800

    idea第三次提交

--- src/C.java
+++ src/C.java
@@ -1,6 +1,6 @@
 public class C {
     public static void main(String[] args) {
-        System.out.println("aaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaccccccccccccccc");
 
 
     }
